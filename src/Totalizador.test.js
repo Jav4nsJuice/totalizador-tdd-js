eb9c02b527d4b7f867a83c869478cd81464f69bd
Jav4nsJuice <javans.ferrel@gmail.com>
2021-09-30 10:10:30 -0400

    Calcula total incluyendo todos los descuentos

--- src/Totalizador.test.js
+++ src/Totalizador.test.js
@@ -23,6 +23,21 @@ describe("Totalizador ", () => {
   it("deberia calcular para un estado por defecto", () => {
     expect(calcularTotal(3, 2, "AL")).toEqual(6.24);
   });
+  it("deberia calcular para un descuento para 1000 o mas", () => {
+    expect(calcularTotal(500, 2.2, "AL")).toEqual(1109.68);
+  });
+  it("deberia calcular para un descuento para 3000 o mas", () => {
+    expect(calcularTotal(1000, 3, "AL")).toEqual(2964);
+  });
+  it("deberia calcular para un descuento para 7000 o mas", () => {
+    expect(calcularTotal(2000, 4, "AL")).toEqual(7737.6);
+  });
+  it("deberia calcular para un descuento para 10000 o mas", () => {
+    expect(calcularTotal(2000, 6, "AL")).toEqual(11232);
+  });
+  it("deberia calcular para un descuento para 30000 o mas", () => {
+    expect(calcularTotal(15000, 3, "AL")).toEqual(39780);
+  });
 });
 
 function impuestoEstado(estado) {
@@ -30,9 +45,26 @@ function impuestoEstado(estado) {
   return impuestos[estado];
 }
 
+function descuentoPorSubTotal(subTotal){
+  if (subTotal >= 1000 && subTotal < 3000){
+    return 0.03;
+  }else if(subTotal >= 3000 && subTotal < 7000){
+    return 0.05;
+  }else if(subTotal >= 7000 && subTotal < 10000){
+    return 0.07;
+  }else if(subTotal >= 10000 && subTotal < 30000){
+    return 0.1;
+  }else if(subTotal >= 30000){
+    return 0.15;
+  }
+  return 0;
+}
+
 function calcularTotal(cantidad, precio, estado) {
   let subTotal = cantidad * precio;
   let impuesto = impuestoEstado(estado);
   subTotal = subTotal + subTotal * impuesto;
+  let descuento = descuentoPorSubTotal(subTotal);
+  subTotal = subTotal - subTotal * descuento;
   return subTotal;
 }
